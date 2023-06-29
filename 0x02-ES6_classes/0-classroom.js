61a87427ccc2d151b41cd01464ba4cee581087df
clintmadeit <clintonbrirdley@gmail.com>
2023-06-29 21:29:22 +0300

    maxSize

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,8 +1,6 @@
 export default class ClassRoom {
-    constructor(_maxStudentsSize, teacher, roomNumber) {
-        this.name = _maxStudentsSize;
-        this.name = teacher;
-        this.name = roomNumber;
-    }   
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
 }
-console.log(_maxStudentsSize.name);
+console.log(ClassRoom.name);
