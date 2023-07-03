e4b3bb4440481c06867ae72da9f10f7a3d041977
clintmadeit <clintonbrirdley@gmail.com>
2023-07-03 16:10:56 +0300

    task _1_and_2

--- 0-get_list_students.js
+++
@@ -1,9 +0,0 @@
-export default function getListStudents( {
-    return[
-        {id: 1, firstname: 'Guillaume', location: 'San Francisco'},
-        {id: 2, firstname: 'James', location: 'Columbia'},
-        {id: 5, firstname: 'Serena', location: 'San Francisco'}
-    ];
-});
-
-console.log(getListStudents());
