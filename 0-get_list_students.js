608dbf86e2dd4c2e666d02d9478320d61f50594b
clintmadeit <clintonbrirdley@gmail.com>
2023-07-03 15:08:33 +0300

    packages

---
+++ 0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents( {
+    return[
+        {id: 1, firstname: 'Guillaume', location: 'San Francisco'},
+        {id: 2, firstname: 'James', location: 'Columbia'},
+        {id: 5, firstname: 'Serena', location: 'San Francisco'}
+    ];
+});
+
+console.log(getListStudents());
